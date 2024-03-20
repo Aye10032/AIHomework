8b6efd92aecee9510ab027337df3665f7da9df06
Aye10032 <aye10032@gmail.com>
2024-03-20 21:05:58 +0800

    调整参数

--- Machine-Learning-in-Bioinformatics/code1/TrainRNN.py
+++ Machine-Learning-in-Bioinformatics/code1/TrainRNN.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 from LoadData import OneHotDataset, MyDataset
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-writer = SummaryWriter()
+print(DEVICE)
 
 
 class RNN(nn.Module):
@@ -19,48 +19,39 @@ class RNN(nn.Module):
         self.hidden_size = hidden_size
 
         self.word_embeddings = nn.Embedding(input_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_size)
+        self.rnn = nn.RNN(embedding_dim, hidden_size, num_layers=2)
         self.fc = nn.Linear(hidden_size, output_size)
-
-        self.hidden = self.init_hidden()
-
-    def init_hidden(self):
-        h0 = torch.zeros(1, self.hidden_size).to(DEVICE)
-        c0 = torch.zeros(1, self.hidden_size).to(DEVICE)
-
-        return h0, c0
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
         embeds = self.word_embeddings(x)
-        # print(embeds.shape)
-        lstm_out, self.hidden = self.lstm(embeds, self.hidden)
-        # print(lstm_out.shape)
-        out = self.fc(lstm_out) # .view(x.shape[0], -1)
-        out = F.log_softmax(out, dim=1)
-
+        rnn_out, _ = self.rnn(embeds)
+        out = self.fc(rnn_out)
+        out = self.softmax(out)
         return out
 
 
 INPUT_SIZE = 20
-EMBEDDING_DIM = 32
-HIDDEN_SIZE = 15
-NUM_LAYERS = 3
+EMBEDDING_DIM = 64
+HIDDEN_SIZE = 250
 OUTPUT_SIZE = 3
-LEARNING_RATE = 0.1
-EPOCH = 10
+LEARNING_RATE = 0.01
+EPOCH = 5
+
+writer = SummaryWriter(log_dir=f'runs/RNN_embedding{EMBEDDING_DIM}_hidden{HIDDEN_SIZE}_LR{LEARNING_RATE}')
 
 dataset = MyDataset(DEVICE)
-train_size = int(len(dataset) * 0.7)
+train_size = int(len(dataset) * 0.8)
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True)
 
 model = RNN(EMBEDDING_DIM, HIDDEN_SIZE, INPUT_SIZE, OUTPUT_SIZE).to(DEVICE)
 
 with torch.no_grad():
     print(model)
-    # writer.add_graph(model, input_to_model=dataset.__getitem__(0)[0])
+    writer.add_graph(model, input_to_model=dataset.__getitem__(0)[0].squeeze())
 
 loss_function = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
@@ -69,9 +60,10 @@ length = len(train_loader)
 for epoch in range(EPOCH):
     model.train()
     train_loss = 0
+    train_correct = 0
+    train_total = 0
     for i, (sequences, labels) in tqdm(enumerate(train_loader), total=length, desc=f'epoch{epoch}'):
         model.zero_grad()
-        model.hidden = model.init_hidden()
         sequences, labels = sequences.squeeze(), labels.squeeze()
 
         outputs = model(sequences)
@@ -80,28 +72,35 @@ for epoch in range(EPOCH):
         loss.backward()
         optimizer.step()
 
+        train_total += labels.shape[0]
+        predicted = torch.argmax(outputs.data, dim=1)
+        train_correct += torch.sum(predicted == labels).item()
         train_loss += loss.item()
 
-    writer.add_scalar('Loss', train_loss / len(train_loader), epoch)
-
-    model.eval()
-    correct = 0
-    total = 0
-    for test_sequences, test_labels in test_loader:
-        test_sequences, test_labels = test_sequences.squeeze(), test_labels.squeeze()
-        # print(test_sequences.shape)
-        outputs = model(test_sequences)
-        # print(outputs.shape)
-        predicted = torch.argmax(outputs.data, dim=1)
-        total += test_labels.shape[0]
-        # print(predicted.shape, test_labels.shape)
-        correct += torch.sum(predicted == test_labels).item()
-        # print(correct, total)
-
-    writer.add_scalar('Accuracy', correct / total, epoch)
+    writer.add_scalar('Train/Acc', train_correct / train_total, epoch)
+    writer.add_scalar('Train/Loss', train_loss / len(train_loader), epoch)
     writer.flush()
 
-    if epoch % 10 == 0:
-        logger.info(f'Accuracy: {100 * correct / total}%, Loss: {train_loss / len(train_loader)}')
+    model.eval()
+    test_loss = 0
+    test_correct = 0
+    test_total = 0
+    for test_sequences, test_labels in test_loader:
+        test_sequences, test_labels = test_sequences.squeeze(), test_labels.squeeze()
+        outputs = model(test_sequences)
+
+        loss = loss_function(outputs, test_labels)
+
+        test_total += test_labels.shape[0]
+        predicted = torch.argmax(outputs.data, dim=1)
+        test_correct += torch.sum(predicted == test_labels).item()
+        test_loss += loss.item()
+
+    writer.add_scalar('Test/Acc', test_correct / test_total, epoch)
+    writer.add_scalar('Test/Loss', test_loss / len(test_loader), epoch)
+    writer.flush()
+
+    if epoch % 9 == 0 and epoch != 0:
+        logger.info(f'Accuracy: {100 * test_correct / test_total}%, Loss: {test_loss / len(test_loader)}')
 
 writer.close()
