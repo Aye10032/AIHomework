d7d512450210ac85501161e091751ad77e9d6df3
Aye10032 <aye10032@gmail.com>
2024-03-27 20:13:05 +0800

    直方图绘制

--- Biological-Image-Processing-and-Informatics/homework01/01read_show.py
+++ Biological-Image-Processing-and-Informatics/homework01/01read_show.py
@@ -1,6 +1,17 @@
 import cv2
+from matplotlib import pyplot as plt
 
 img = cv2.imread('axon01.tif', cv2.IMREAD_UNCHANGED)
+
+min_gray = img.min()
+max_gray = img.max()
+print(min_gray, max_gray)
+plt.hist(img.reshape(-1), bins=150, color='skyblue', edgecolor='black', range=(min_gray, max_gray))
+
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+plt.show()
 
 img_out = img.astype('uint8')
 cv2.imshow('test', img_out)
