3dc5351776c30d0b2657b7081a36dc0d2b3e2de3
Aye10032 <aye10032@gmail.com>
2024-04-20 01:09:22 +0800

    绘制拟合图案

--- Python-for-Scientific-Computation-and-Data-Analysis/homework2/02CurveFit.py
+++ Python-for-Scientific-Computation-and-Data-Analysis/homework2/02CurveFit.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
 a_real = 1
@@ -15,5 +16,14 @@ y = gauss(x_data, a_real, b_real, c_real)
 y_noise = y + 0.2 * np.random.normal(size=x_data.size)
 
 popt, pcov = curve_fit(gauss, x_data, y_noise, p0=[1, 1, 1])
-
 print(popt)
+
+a_fit, b_fit, c_fit = popt
+y_fit = gauss(x_data, a_fit, b_fit, c_fit)
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_data, y, label='Real Data', linestyle='--')
+plt.scatter(x_data, y_noise, color='red', label='Noisy Data', alpha=0.5)
+plt.plot(x_data, y_fit, label='Fit Result', linestyle='-', linewidth=2)
+plt.legend()
+plt.show()
