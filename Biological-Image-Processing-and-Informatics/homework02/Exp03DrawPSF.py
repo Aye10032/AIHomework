62068cbfa25e317275668ea381a170a06b64e664
Aye10032 <aye10032@gmail.com>
2024-04-20 23:42:03 +0800

    添加高斯函数

--- Biological-Image-Processing-and-Informatics/homework02/Exp03DrawPSF.py
+++ Biological-Image-Processing-and-Informatics/homework02/Exp03DrawPSF.py
@@ -1,9 +1,10 @@
 import numpy as np
+import seaborn as sns
 from matplotlib import pyplot as plt
 from scipy.special import j1
 
 
-def psf(r, light_wave_l, na):
+def psf(r: np.ndarray, light_wave_l, na) -> np.ndarray:
     k = 2 * np.pi / light_wave_l
     rho = k * na * r
 
@@ -14,13 +15,22 @@ def psf(r, light_wave_l, na):
     return z
 
 
+def gauss(_x, sigma):
+    return np.exp(-x ** 2 / (2 * sigma ** 2))
+
+
 LAMBDA = 0.48
 NA = 0.5
 
 x = np.linspace(-2, 2, 200)
 y = psf(x, LAMBDA, NA)
 r0 = 0.61 * LAMBDA / NA
+y2 = gauss(x, r0 / 3)
 
-plt.plot(x, y)
-plt.axvline(x=r0, linestyle='--', color='r')
+sns.set_style('whitegrid')
+fig, ax = plt.subplots()
+sns.lineplot(x=x, y=y, ax=ax)
+sns.lineplot(x=x, y=y2, ax=ax)
+ax.axvline(x=r0, color='red', linestyle='--')
+
 plt.show()
