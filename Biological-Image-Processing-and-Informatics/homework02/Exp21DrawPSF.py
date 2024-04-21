2bd4b3e62cd8da729a867d096a3e549c557c0e15
Aye10032 <aye10032@gmail.com>
2024-04-22 00:42:29 +0800

    draw Airy disk

--- Biological-Image-Processing-and-Informatics/homework02/Exp21DrawPSF.py
+++ Biological-Image-Processing-and-Informatics/homework02/Exp21DrawPSF.py
@@ -3,6 +3,9 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from scipy.special import j1
 
+LAMBDA = 0.48
+NA = 0.5
+
 
 def psf(r: np.ndarray, light_wave_l: float, na: float) -> np.ndarray:
     """
@@ -27,18 +30,20 @@ def gauss(_x: np.ndarray, sigma: float) -> np.ndarray:
     return np.exp(-_x ** 2 / (2 * sigma ** 2))
 
 
-LAMBDA = 0.48
-NA = 0.5
+def main() -> None:
+    x = np.linspace(-1.8, 1.8, 200)
+    y = psf(x, LAMBDA, NA)
+    r0 = 0.61 * LAMBDA / NA
+    y2 = gauss(x, r0 / 3)
 
-x = np.linspace(-1.8, 1.8, 200)
-y = psf(x, LAMBDA, NA)
-r0 = 0.61 * LAMBDA / NA
-y2 = gauss(x, r0 / 3)
+    sns.set_style('whitegrid')
+    fig, ax = plt.subplots()
+    sns.lineplot(x=x, y=y2, ax=ax, color='orange')
+    sns.lineplot(x=x, y=y, ax=ax, color='b')
+    ax.axvline(x=r0, color='red', linestyle='--')
 
-sns.set_style('whitegrid')
-fig, ax = plt.subplots()
-sns.lineplot(x=x, y=y2, ax=ax, color='orange')
-sns.lineplot(x=x, y=y, ax=ax, color='b')
-ax.axvline(x=r0, color='red', linestyle='--')
+    plt.show()
 
-plt.show()
+
+if __name__ == '__main__':
+    main()
