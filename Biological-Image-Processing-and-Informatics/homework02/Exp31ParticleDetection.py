0287a9a1d7834594a0dac482e17eeefd9b056dfb
Aye10032 <aye10032@gmail.com>
2024-04-26 10:29:02 +0800

    局部极值检测

--- Biological-Image-Processing-and-Informatics/homework02/Exp31ParticleDetection.py
+++ Biological-Image-Processing-and-Informatics/homework02/Exp31ParticleDetection.py
@@ -1,10 +1,11 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import cv2
 import numpy as np
 
 from loguru import logger
 from Exp11GaussianKernel import conv2d, PaddingMode
+from utils.Decorator import timer
 
 
 def get_background_noise(input_mat: np.ndarray) -> Tuple[float, float]:
@@ -26,6 +27,7 @@ def get_background_noise(input_mat: np.ndarray) -> Tuple[float, float]:
     return mean, standard_deviation
 
 
+@timer
 def gauss_filter(input_mat: np.ndarray, wavelength: float, NA: float) -> np.ndarray:
     """
     使用高斯滤波器对输入的图像进行滤波处理。
@@ -51,6 +53,28 @@ def gauss_filter(input_mat: np.ndarray, wavelength: float, NA: float) -> np.ndar
     return output_mat
 
 
+@timer
+def local_min_max(input_mat: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    寻找输入矩阵中的局部最小值和局部最大值。
+
+    :param input_mat: 输入的二维numpy数组，寻找其中的局部最小和局部最大值。
+    :return: 返回一个元组，包含两个元素。第一个元素是标记局部最大值的二维数组（255表示局部最大值），第二个元素是标记局部最小值的二维数组（255表示局部最小值）。
+    """
+
+    local_max = np.zeros(input_mat.shape, dtype=np.uint16)
+    local_min = np.zeros(input_mat.shape, dtype=np.uint16)
+    kernel_size = 3
+
+    for i in range(kernel_size, input_mat.shape[0] - kernel_size):
+        for j in range(kernel_size, input_mat.shape[1] - kernel_size):
+            roi = input_mat[i - kernel_size:i + kernel_size + 1, j - kernel_size:j + kernel_size + 1]
+            local_min[i, j] = (input_mat[i, j] == roi.min()) * 255
+            local_max[i, j] = (input_mat[i, j] == roi.max()) * 255
+
+    return local_max, local_min
+
+
 def main() -> None:
     init_src = cv2.imread('image/BIP_Project02_image_sequence/001_a5_002_t001.tif', cv2.IMREAD_UNCHANGED)
 
@@ -58,6 +82,11 @@ def main() -> None:
 
     src_gauss = gauss_filter(init_src, 5.15, 1.4)
     cv2.imwrite('image/BIP_Project02_image_sequence/gauss/001_a5_002_t001.tif', src_gauss.astype('uint8'))
+
+    logger.info('start local minima and maxima detect')
+    local_maxima, local_minima = local_min_max(src_gauss)
+    cv2.imwrite('image/BIP_Project02_image_sequence/minmax/001_a5_002_t001_max.tif', local_maxima.astype('uint8'))
+    cv2.imwrite('image/BIP_Project02_image_sequence/minmax/001_a5_002_t001_min.tif', local_minima.astype('uint8'))
 
     # cv2.waitKey(0)
 
