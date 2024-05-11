bb2ac81a6473c4e5cad88dca67249fa1141ec76b
Aye10032 <aye10032@gmail.com>
2024-05-11 19:17:31 +0800

    修改路径

--- Intelligent-Computing-System/pe_exp_student/sim/tb_top_1.v
+++ Intelligent-Computing-System/pe_exp_student/sim/tb_top_1.v
@@ -30,10 +30,10 @@ reg [ 31:0] result[3:0];
 
 initial
 begin
-  $readmemh("D:/pe_exp/data/inst", inst);
-  $readmemh("D:/pe_exp/data/neuron", neuron);
-  $readmemh("D:/pe_exp/data/weight", weight);
-  $readmemb("D:/pe_exp/data/result", result);
+  $readmemh("D:/program/github/AIHomework/Intelligent-Computing-System/pe_exp_student/data/inst", inst);
+  $readmemh("D:/program/github/AIHomework/Intelligent-Computing-System/pe_exp_student/data/neuron", neuron);
+  $readmemh("D:/program/github/AIHomework/Intelligent-Computing-System/pe_exp_student/data/weight", weight);
+  $readmemb("D:/program/github/AIHomework/Intelligent-Computing-System/pe_exp_student/data/result", result);
 end
 
 reg [ 1:0]   inst_addr;
