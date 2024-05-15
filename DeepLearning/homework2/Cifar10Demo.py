afeca316ec2d850d146afcae28016e978157f577
Aye10032 <aye10032@gmail.com>
2024-05-15 19:00:42 +0800

    调整参数，多卡并行训练

--- DeepLearning/homework2/Cifar10Demo.py
+++ DeepLearning/homework2/Cifar10Demo.py
@@ -1,6 +1,7 @@
 import os
 
 import torch
+import torch.distributed as dist
 from torch import nn, Tensor
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import (
@@ -17,7 +18,7 @@ from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from VitModel import ViT, DEVICE, ChannelSelection
+from VitModel import ViT, ChannelSelection
 
 
 def sparse_selection(net: nn.Module):
@@ -31,6 +32,7 @@ def train(
         net: nn.Module,
         optimizer: torch.optim.Optimizer,
         schedule: torch.optim.lr_scheduler.CosineAnnealingLR,
+        device: list[torch.device],
         epoch: int,
         train_loader: DataLoader,
         writer: SummaryWriter
@@ -43,12 +45,18 @@ def train(
     output_embed = torch.empty((0, 10))
     target_embeds = torch.empty(0)
 
-    loop = tqdm(enumerate(train_loader), total=len(train_loader), desc=f'Epoch {epoch}')
+    train_loader.sampler.set_epoch(epoch)
+
+    if dist.get_rank() == 0:
+        loop = tqdm(enumerate(train_loader), total=len(train_loader), desc=f'Epoch {epoch}')
+    else:
+        loop = enumerate(train_loader)
+
     for batch_idx, (inputs, targets) in loop:
         inputs: Tensor
         targets: Tensor
 
-        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+        inputs, targets = inputs.to(device[0]), targets.to(device[-1])
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, targets)
@@ -62,7 +70,8 @@ def train(
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        loop.set_postfix(loss=train_loss / (batch_idx + 1), acc=100. * correct / total)
+        if dist.get_rank() == 0:
+            loop.set_postfix(loss=train_loss / (batch_idx + 1), acc=100. * correct / total)
 
         if batch_idx <= 3:
             output_embed = torch.cat((output_embed, outputs.clone().cpu()), 0)
@@ -70,38 +79,43 @@ def train(
 
     writer.add_scalar('lr', schedule.get_last_lr()[0], epoch)
 
-    if epoch % 9 == 0:
-        writer.add_embedding(
-            output_embed,
-            metadata=target_embeds,
-            global_step=epoch + 1,
-            tag='cifar10'
-        )
+    if dist.get_rank() == 0:
+        if epoch % 9 == 0:
+            writer.add_embedding(
+                output_embed,
+                metadata=target_embeds,
+                global_step=epoch + 1,
+                tag='cifar10'
+            )
 
-    writer.add_scalar('Train/loss', train_loss / (batch_idx + 1), epoch)
-    writer.add_scalar('Train/acc', 100. * correct / total, epoch)
+        writer.add_scalar('Train/loss', train_loss / (batch_idx + 1), epoch)
+        writer.add_scalar('Train/acc', 100. * correct / total, epoch)
 
-    for name, param in net.named_parameters():
-        layer, attr = os.path.splitext(name)
-        attr = attr[1:]
-        writer.add_histogram('{}/{}'.format(layer, attr), param.clone().cpu().data.numpy(), epoch)
+        for name, param in net.named_parameters():
+            layer, attr = os.path.splitext(name)
+            attr = attr[1:]
+            writer.add_histogram('{}/{}'.format(layer, attr), param.clone().cpu().data.numpy(), epoch)
 
-    writer.flush()
+        writer.flush()
 
 
-def test(net: nn.Module, epoch: int, test_loader: DataLoader, writer: SummaryWriter):
+def test(net: nn.Module, device: list[torch.device], epoch: int, test_loader: DataLoader, writer: SummaryWriter):
     criterion = nn.CrossEntropyLoss()
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
-        loop = tqdm(enumerate(test_loader), total=len(test_loader), desc=f'Test {epoch}')
+        if dist.get_rank() == 0:
+            loop = tqdm(enumerate(test_loader), total=len(test_loader), desc=f'Test {epoch}')
+        else:
+            loop = enumerate(test_loader)
+
         for batch_idx, (inputs, targets) in loop:
             inputs: Tensor
             targets: Tensor
 
-            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+            inputs, targets = inputs.to(device[0]), targets.to(device[-1])
             outputs = net(inputs)
             loss = criterion(outputs, targets)
 
@@ -110,17 +124,16 @@ def test(net: nn.Module, epoch: int, test_loader: DataLoader, writer: SummaryWri
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            loop.set_postfix(loss=test_loss / (batch_idx + 1), acc=100. * correct / total)
+            if dist.get_rank() == 0:
+                loop.set_postfix(loss=test_loss / (batch_idx + 1), acc=100. * correct / total)
 
-        writer.add_scalar('Test/loss', test_loss / (batch_idx + 1), epoch)
-        writer.add_scalar('Test/acc', 100. * correct / total, epoch)
-        writer.flush()
+        if dist.get_rank() == 0:
+            writer.add_scalar('Test/loss', test_loss / (batch_idx + 1), epoch)
+            writer.add_scalar('Test/acc', 100. * correct / total, epoch)
+            writer.flush()
 
 
-def main() -> None:
-    lr = 1e-4
-    max_epoch = 500
-
+def load_data():
     trans_train = Compose([
         RandomResizedCrop(224),
         RandomHorizontalFlip(),
@@ -133,8 +146,7 @@ def main() -> None:
     ])
 
     trans_valid = Compose([
-        Resize(256),
-        CenterCrop(224),
+        Resize(224),
         ToTensor(),
         Normalize(
             mean=[0.485, 0.456, 0.406],
@@ -145,35 +157,58 @@ def main() -> None:
     train_set = CIFAR10(root='./cifar10', train=True, download=True, transform=trans_train)
     test_set = CIFAR10(root='./cifar10', train=False, download=False, transform=trans_valid)
 
-    train_loader = DataLoader(train_set, batch_size=256, shuffle=True, num_workers=2)
-    test_loader = DataLoader(test_set, batch_size=256, shuffle=False, num_workers=2)
+    train_sample = torch.utils.data.distributed.DistributedSampler(train_set)
+    test_sample = torch.utils.data.distributed.DistributedSampler(test_set)
+
+    train_loader = DataLoader(train_set, batch_size=256, shuffle=False, num_workers=2, sampler=train_sample)
+    test_loader = DataLoader(test_set, batch_size=256, shuffle=False, num_workers=2, sampler=test_sample)
+
+    return train_loader, test_loader
+
+
+def main() -> None:
+    dist.init_process_group(backend="nccl")
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
+
+    lr = 1e-4
+    max_epoch = 500
+
+    train_loader, test_loader = load_data()
+
+    local_rank = torch.distributed.get_rank()
+    torch.cuda.set_device(local_rank)
+    device = torch.device("cuda", local_rank)
 
     net = ViT(
-        image_size=256,
-        patch_size=32,
+        image_size=(224, 224),
+        patch_size=(32, 32),
         num_classes=10,
-        dim=512,
-        depth=6,
-        heads=8,
-        mlp_dim=128,
-        dropout=.1,
-        emb_dropout=.1
-    ).to(DEVICE)
-    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+        dim=256,
+        layers=3,
+        heads=6,
+        hidden_size=64,
+        mlp_size=128,
+        dropout=0.,
+        emb_dropout=0.,
+        device=[device]
+    )
+    ddp_net = nn.parallel.DistributedDataParallel(net, device_ids=[local_rank], output_device=local_rank, find_unused_parameters=True)
+    optimizer = torch.optim.Adam(ddp_net.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epoch * len(train_loader), eta_min=1e-5)
-    writer = SummaryWriter(log_dir=f'runs/cif10_lr{lr}_ep{max_epoch}')
+    writer = SummaryWriter(log_dir=f'runs/cif10_lr{lr}_head6_layer3_dim256_ep500')
 
     with torch.no_grad():
-        writer.add_graph(net, input_to_model=train_set.__getitem__(0)[0].unsqueeze(0).to(DEVICE))
+        tensor = torch.rand(1, 3, 224, 224).to(device)
+        writer.add_graph(net, input_to_model=tensor)
 
     for i in range(max_epoch):
-        train(net, optimizer, scheduler, i, train_loader, writer)
+        train(net, optimizer, scheduler, [device], i, train_loader, writer)
 
         if i % 5 == 0:
-            test(net, i, test_loader, writer)
+            test(net, [device], i, test_loader, writer)
 
-        if i % 50 == 0 and i != 0:
-            torch.save(net.state_dict(), f'models/cifar10_lr{lr}_ep{i}.pth')
+        if i % 50 == 0 and i != 0 and local_rank == 0:
+            torch.save(net.state_dict(), f'models/cifar10_head6_layer3_dim256_ep{i}.pth')
 
 
 if __name__ == '__main__':
