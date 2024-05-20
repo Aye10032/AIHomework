bbbe82f4d9ed904cb1ec9008e6a0c64b7d3127b9
Aye10032 <aye10032@gmail.com>
2024-05-20 17:28:42 +0800

    微调基底模型

--- DeepLearning/homework2/HuggingfaceDemo.py
+++ DeepLearning/homework2/HuggingfaceDemo.py
@@ -1,139 +1,96 @@
-import os.path
-import shutil
-
+import evaluate
 import numpy as np
 import torch
-from accelerate import init_empty_weights
-from matplotlib import pyplot as plt
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
-from torch import Tensor
-from torchvision.transforms import (
-    Compose,
-    Resize,
-    RandomResizedCrop,
-    CenterCrop,
-    RandomHorizontalFlip,
-    RandomRotation,
-    ToTensor,
-    Normalize,
-    ToPILImage
-)
-from torchvision.datasets import CIFAR10
 from datasets import load_dataset
-from transformers import (
-    ViTImageProcessor,
-    ViTForImageClassification,
-    Trainer,
-    TrainingArguments
-)
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from transformers import ViTImageProcessor, ViTForImageClassification, TrainingArguments, Trainer
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+ds = load_dataset('cifar10')
+metric = evaluate.load("accuracy")
 
-# def load_data():
-# if os.path.exists('./cifar10'):
-#     shutil.rmtree('./cifar10')
+labels = ds['train'].features['label'].names
 
-train_data = load_dataset('cifar10', split='train')
-test_set = load_dataset('cifar10', split='test')
-splits = train_data.train_test_split(test_size=0.1)
-train_set = splits['train']
-valid_set = splits['test']
-
-i_to_s = dict((k, v) for k, v in enumerate(train_set.features['label'].names))
-s_to_i = dict((v, k) for k, v in enumerate(train_set.features['label'].names))
+processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
 
 
-def train_trans(args):
-    _trans = Compose([
-        Resize(224),
-        RandomHorizontalFlip(),
-        ToTensor(),
-        Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
-    ])
-    args['pixels'] = [_trans(image.convert('RGB')) for image in args['img']]
-    return args
+def transform(example_batch):
+    # Take a list of PIL images and turn them to pixel values
+    inputs = processor([x for x in example_batch['img']], return_tensors='pt')
+
+    # Don't forget to include the labels!
+    inputs['labels'] = example_batch['label']
+    return inputs
 
 
-def valid_trans(args):
-    _trans = Compose([
-        Resize(224),
-        ToTensor(),
-        Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
-    ])
-    args['pixels'] = [_trans(image.convert('RGB')) for image in args['img']]
-    return args
+def collate_fn(batch):
+    return {
+        'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
+        'labels': torch.tensor([x['labels'] for x in batch])
+    }
 
 
-train_set.set_transform(train_trans)
-valid_set.set_transform(valid_trans)
-test_set.set_transform(valid_trans)
+def compute_metrics(p):
+    return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
 
-processor: ViTImageProcessor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
 
-with init_empty_weights():
-    model: ViTForImageClassification = ViTForImageClassification.from_pretrained(
-        'google/vit-base-patch16-224',
-        num_labels=10,
-        ignore_mismatched_sizes=True,
-        id2label=i_to_s,
-        label2id=s_to_i
+def main() -> None:
+    model = ViTForImageClassification.from_pretrained(
+        'google/vit-base-patch16-224-in21k',
+        num_labels=len(labels),
+        id2label={str(i): c for i, c in enumerate(labels)},
+        label2id={c: str(i) for i, c in enumerate(labels)}
     )
 
-model.tie_weights()
+    prepared_ds = ds.with_transform(transform)
 
-train_args = TrainingArguments(
-    'test-cifar10',
-    save_strategy='epoch',
-    evaluation_strategy='epoch',
-    learning_rate=2e-5,
-    per_device_train_batch_size=64,
-    per_device_eval_batch_size=32,
-    num_train_epochs=3,
-    weight_decay=0.01,
-    load_best_model_at_end=True,
-    metric_for_best_model='accuracy',
-    logging_dir='hf_log',
-    report_to=['tensorboard'],
-    remove_unused_columns=False
-)
+    training_args = TrainingArguments(
+        output_dir="./vit-base-cifar10",
+        per_device_train_batch_size=32,
+        eval_strategy="steps",
+        num_train_epochs=5,
+        fp16=True,
+        save_steps=100,
+        eval_steps=100,
+        logging_steps=10,
+        learning_rate=2e-4,
+        save_total_limit=2,
+        remove_unused_columns=False,
+        push_to_hub=False,
+        report_to='tensorboard',
+        load_best_model_at_end=True,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=collate_fn,
+        compute_metrics=compute_metrics,
+        train_dataset=prepared_ds["train"],
+        eval_dataset=prepared_ds["test"],
+        tokenizer=processor,
+    )
+
+    train_results = trainer.train()
+    trainer.save_model()
+    trainer.log_metrics("train", train_results.metrics)
+    trainer.save_metrics("train", train_results.metrics)
+    trainer.save_state()
+
+    metrics = trainer.evaluate(prepared_ds['test'])
+    trainer.log_metrics("eval", metrics)
+    trainer.save_metrics("eval", metrics)
+
+    outputs = trainer.predict(prepared_ds['test'])
+    y_true = outputs.label_ids
+    y_pred = outputs.predictions.argmax(1)
+
+    _labels = prepared_ds['test'].features['label'].names
+    cm = confusion_matrix(y_true, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=_labels)
+    disp.plot(xticks_rotation=45)
+    plt.savefig('ConfusionMatrix.png')
 
 
-def collate_fn(examples):
-    pixels = torch.stack([example["pixels"] for example in examples])
-    labels = torch.tensor([example["label"] for example in examples])
-    return {"pixel_values": pixels, "labels": labels}
-
-
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    return dict(accuracy=accuracy_score(predictions, labels))
-
-
-trainer = Trainer(
-    model,
-    train_args,
-    train_dataset=train_set,
-    eval_dataset=valid_set,
-    data_collator=collate_fn,
-    compute_metrics=compute_metrics,
-    tokenizer=processor,
-)
-
-trainer.train()
-
-outputs = trainer.predict(test_set)
-y_true = outputs.label_ids
-y_pred = outputs.predictions.argmax(1)
-
-_labels = train_set.features['label'].names
-cm = confusion_matrix(y_true, y_pred)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=_labels)
-disp.plot(xticks_rotation=45)
-plt.savefig('ConfusionMatrix.png')
+if __name__ == '__main__':
+    main()
