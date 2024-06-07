fdf26e00ac3927c3aefe6ae1d1e429b907a052f1
Aye10032 <aye10032@gmail.com>
2024-06-07 17:48:49 +0800

    update loss

--- Biological-Image-Processing-and-Informatics/homework03/losses.py
+++ Biological-Image-Processing-and-Informatics/homework03/losses.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from torchvision.ops import complete_box_iou_loss
 
 __all__ = ['BCELoss', 'DICELoss', 'IoULoss']
 
@@ -8,10 +7,11 @@ __all__ = ['BCELoss', 'DICELoss', 'IoULoss']
 class BCELoss(nn.Module):
     def __init__(self):
         super(BCELoss, self).__init__()
+        self.bce_loss = nn.BCELoss()
 
     def forward(self, input, target):
-        bceloss = nn.BCELoss()
-        return bceloss(input, target)
+        loss = self.bce_loss(input, target)
+        return loss
 
 
 class DICELoss(nn.Module):
@@ -20,19 +20,20 @@ class DICELoss(nn.Module):
 
     def forward(self, input, target, ep=1e-8):
         # you should add the code to compute DICELoss
-        intersection = 2 * torch.sum(input * target) + ep
-        union = torch.sum(input) + torch.sum(target) + ep
-        loss = 1 - intersection / union
+        intersection = 2 * torch.sum(input * target, dim=(1, 2, 3)) + ep
+        union = torch.sum(input, dim=(1, 2, 3)) + torch.sum(target, dim=(1, 2, 3)) + ep
+        loss = (1 - intersection / union).mean()
         return loss
 
 
 class IoULoss(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(IoULoss, self).__init__()
 
     def forward(self, input, target, ep=1e-8):
         # you should add the code to compute IoULoss
-        intersection = torch.logical_and(input, target).sum()
-        union = torch.logical_or(input, target).sum()
-        iou = (intersection + ep) / (union + ep)
-        return 1 - iou
+        intersection = torch.sum(input * target, dim=(1, 2, 3)) + ep
+        union = (torch.sum(input, dim=(1, 2, 3)) - intersection) + ep
+
+        loss = (1 - intersection / union).mean()
+        return loss
