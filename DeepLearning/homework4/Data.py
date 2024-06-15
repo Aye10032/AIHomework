2b7c1ea77489086f43ab6c489561f32bf31abc91
Aye10032 <aye10032@gmail.com>
2024-06-16 02:00:07 +0800

    数据集初始化和词表修改

--- DeepLearning/homework4/Data.py
+++ DeepLearning/homework4/Data.py
@@ -1,9 +1,11 @@
 import os.path
 from enum import IntEnum
 
-import numpy as np
-from torch.utils.data import Dataset
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 from loguru import logger
+from tqdm import tqdm
 
 
 class DataType(IntEnum):
@@ -13,7 +15,7 @@ class DataType(IntEnum):
 
 
 class TransData(Dataset):
-    def __init__(self, data_path: str, data_type: DataType):
+    def __init__(self, data_path: str, data_type: DataType, padding_length: int = 62):
         match data_type:
             case DataType.TRAIN:
                 self.src_file = os.path.join(data_path, 'train.zh')
@@ -27,34 +29,67 @@ class TransData(Dataset):
             case _:
                 raise RuntimeError('无效的参数！')
 
+        self.max_len = padding_length
         self.src_vocab = os.path.join(data_path, 'vocab.zh')
         self.target_vocab = os.path.join(data_path, 'vocab.en')
 
+        self.__build_tokenizer()
         self.__load_data()
 
     def __getitem__(self, item):
-        raise NotImplementedError
+        return self.src_tensor[item], self.target_tensor[item]
+
+    def __len__(self):
+        return self.src_tensor.shape[0]
+
+    def __build_tokenizer(self):
+        logger.info('building tokenizer...')
+        with open(self.src_vocab, 'r', encoding='utf-8') as f:
+            src_lines = [line.strip() for line in f]
+        self.src_id2word = {index: v for index, v in enumerate(src_lines)}
+        self.src_word2id = {v: index for index, v in enumerate(src_lines)}
+
+        with open(self.target_vocab, 'r', encoding='utf-8') as f:
+            target_lines = [line.strip() for line in f]
+        self.target_id2word = {index: v for index, v in enumerate(target_lines)}
+        self.target_word2id = {v: index for index, v in enumerate(target_lines)}
 
     def __load_data(self):
         logger.info('loading dataset...')
         with open(self.src_file, 'r', encoding='utf-8') as f:
-            src_lines = [line.strip().split(' ') for line in f]
+            src_lines = [['<SOB>'] + line.strip().split(' ') + ['<EOB>'] for line in f]
 
         with open(self.target_file, 'r', encoding='utf-8') as f:
-            target_lines = [line.strip().split(' ') for line in f]
+            target_lines = [['<SOB>'] + line.strip().split(' ') + ['<EOB>'] for line in f]
 
-        self.max_len = 0
-        for line in src_lines:
-            self.max_len = max(len(line), self.max_len)
+        logger.info('tokenize src...')
+        src_ids = []
+        for line in tqdm(src_lines):
+            src_ids.append(torch.tensor([
+                self.src_word2id[word]
+                if word in self.src_word2id else
+                self.src_word2id['<UNK>']
+                for word in line
+            ]))
 
-        for line in target_lines:
-            self.max_len = max(len(line), self.max_len)
+        target_ids = []
+        for line in tqdm(target_lines):
+            target_ids.append(torch.tensor([
+                self.target_word2id[word]
+                if word in self.target_word2id else
+                self.target_word2id['<UNK>']
+                for word in line
+            ]))
 
-        return src_lines, target_lines
+        logger.info(f'padding to {self.max_len}')
+        self.src_tensor = pad_sequence(src_ids, True, self.src_word2id['<PAD>'])
+        self.target_tensor = pad_sequence(target_ids, True, self.target_word2id['<PAD>'])
 
 
 def main() -> None:
     dataset = TransData('data', DataType.TRAIN)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=False, num_workers=2)
+    print(next(iter(dataloader)))
 
 
 if __name__ == '__main__':
